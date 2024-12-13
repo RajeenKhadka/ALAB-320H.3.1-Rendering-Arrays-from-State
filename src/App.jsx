116b89b5cd8006d8af127b99c2f1ff201508b9f7
RajeenKhadka <rajeenkhadka315@gmail.com>
2024-12-12 23:36:23 -0500

    .map is working

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Learner from "./components/Learner";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Learner</h1>
+      <Learner />
+    </>
+  );
+}
+
+export default App;
